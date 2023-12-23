ad9a5fe1c751fd2921e64db5b75a98b969f3a780
cipher <devadigaranjith12@gmail.com>
2023-12-23 16:26:56 +0530

    last

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express")
+
+const serverless = require("serverless-http");
 const path = require("path")
 const app = express();
 const connectMongo =require("./config/mongose")
